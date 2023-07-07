380eedce25ac224dde406a309d1092334f7fdd1e
adairo <adair.reyes450@gmail.com>
2023-07-07 17:23:52 -0600

    create first migration file to define the db schema

---
+++ migrations/20230707225821252_initial-schema.ts
@@ -0,0 +1,25 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import { MigrationBuilder, ColumnDefinitions } from "node-pg-migrate";
+
+export const shorthands: ColumnDefinitions | undefined = undefined;
+
+export async function up(pgm: MigrationBuilder): Promise<void> {
+  pgm.createType("user_roles", ["regular", "admin"]);
+  pgm.createTable("users", {
+    id: { type: "serial" },
+    email: { type: "varchar(60)", notNull: true, unique: true },
+    role: { type: "user_roles", notNull: true },
+  });
+
+  pgm.createType("car_colors", ["red", "blue"]);
+  pgm.createTable("cars", {
+    id: "serial",
+    plate: { type: "varchar(10)", notNull: true, unique: true },
+    vendor: { type: "varchar(20)", notNull: true}, // marca del auto
+    model: { type: "varchar(30)", notNull: true},
+    color: {type: "car_colors", notNull: true},
+    userId: {type: "int", references: "users"}
+  });
+}
+
+// export async function down(pgm: MigrationBuilder): Promise<void> {}
