1f4e83fe0afa14c8e42d81a5a42f0fcc8014bcc5
adairo <adair.reyes450@gmail.com>
2023-07-07 17:29:21 -0600

    fix type of tables id on first migration

--- migrations/20230707225821252_initial-schema.ts
+++ migrations/20230707225821252_initial-schema.ts
@@ -6,19 +6,19 @@ export const shorthands: ColumnDefinitions | undefined = undefined;
 export async function up(pgm: MigrationBuilder): Promise<void> {
   pgm.createType("user_roles", ["regular", "admin"]);
   pgm.createTable("users", {
-    id: { type: "serial" },
+    id: { type: "serial", primaryKey: true },
     email: { type: "varchar(60)", notNull: true, unique: true },
     role: { type: "user_roles", notNull: true },
   });
 
   pgm.createType("car_colors", ["red", "blue"]);
   pgm.createTable("cars", {
-    id: "serial",
+    id: { type: "serial", primaryKey: true },
     plate: { type: "varchar(10)", notNull: true, unique: true },
-    vendor: { type: "varchar(20)", notNull: true}, // marca del auto
-    model: { type: "varchar(30)", notNull: true},
-    color: {type: "car_colors", notNull: true},
-    userId: {type: "int", references: "users"}
+    vendor: { type: "varchar(20)", notNull: true }, // marca del auto
+    model: { type: "varchar(30)", notNull: true },
+    color: { type: "car_colors", notNull: true },
+    userId: { type: "int", references: "users" },
   });
 }
 
