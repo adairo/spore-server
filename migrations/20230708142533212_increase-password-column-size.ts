73b16a1dd316d86bf00314e18be053cdbd4726c1
adairo <adair.reyes450@gmail.com>
2023-07-08 08:30:18 -0600

    increase the string length of users.password column

---
+++ migrations/20230708142533212_increase-password-column-size.ts
@@ -0,0 +1,16 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import { MigrationBuilder, ColumnDefinitions } from "node-pg-migrate";
+
+export const shorthands: ColumnDefinitions | undefined = undefined;
+
+export async function up(pgm: MigrationBuilder): Promise<void> {
+  pgm.alterColumn("users", "password", {
+    type: "varchar(100)",
+  });
+}
+
+export async function down(pgm: MigrationBuilder): Promise<void> {
+  pgm.alterColumn("users", "password", {
+    type: "varchar(50)",
+  });
+}
