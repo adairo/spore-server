bf50c01e08d36466fedf2bffa6dc586112a7007f
adairo <adair.reyes450@gmail.com>
2023-07-08 08:18:05 -0600

    add password field to users table

---
+++ migrations/20230708013720577_add-users-password.ts
@@ -0,0 +1,10 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import { MigrationBuilder, ColumnDefinitions } from "node-pg-migrate";
+
+export const shorthands: ColumnDefinitions | undefined = undefined;
+
+export async function up(pgm: MigrationBuilder): Promise<void> {
+  pgm.addColumn("users", {
+    password: { type: "varchar(50)", notNull: true },
+  });
+}
