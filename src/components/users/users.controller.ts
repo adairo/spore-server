ba7325481377d5da659c2a189edd47c77d2b214b
adairo <adair.reyes450@gmail.com>
2023-07-08 08:25:04 -0600

    use bcryptjs to encrypt the user password before persisting

--- src/components/users/users.controller.ts
+++ src/components/users/users.controller.ts
@@ -3,16 +3,26 @@ import { getValidated } from "../../validate";
 import { CreateUserPayload } from "./users.schema";
 import { getUserByEmail } from "./users.database";
 import * as database from "./users.database";
+import bcrypt from "bcryptjs";
 
 export const createUser: RequestHandler = async (req, res) => {
-  const data = getValidated<CreateUserPayload>(req);
+  const { body: data } = getValidated<CreateUserPayload>(req);
 
-  const existentUser = await getUserByEmail(data.body.email);
+  const existentUser = await getUserByEmail(data.email);
 
+  // verify that the user doesn't exist already in the db
   if (existentUser) {
     return res.status(400).json({ error: "Este correo ya ha sido registrado" });
   }
 
-  const user = await database.createUser(data.body);
+  // Encrypt password
+  const encryptedPassword = await bcrypt.hash(data.password, 10);
+  console.log(encryptedPassword)
+
+  // insert the user
+  const user = await database.createUser({
+    ...data,
+    password: encryptedPassword,
+  });
   res.status(200).json(user);
 };
