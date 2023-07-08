38c85abfa864517180284434293d8c6994c1c3ac
adairo <adair.reyes450@gmail.com>
2023-07-08 08:55:38 -0600

    use jwt to generate a token and send it to the user when created

--- src/components/users/users.controller.ts
+++ src/components/users/users.controller.ts
@@ -4,25 +4,49 @@ import { CreateUserPayload } from "./users.schema";
 import { getUserByEmail } from "./users.database";
 import * as database from "./users.database";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export const createUser: RequestHandler = async (req, res) => {
   const { body: data } = getValidated<CreateUserPayload>(req);
 
-  const existentUser = await getUserByEmail(data.email);
+  try {
+    const existentUser = await getUserByEmail(data.email);
 
-  // verify that the user doesn't exist already in the db
-  if (existentUser) {
-    return res.status(400).json({ error: "Este correo ya ha sido registrado" });
+    // verify that the user doesn't exist already in the db
+    if (existentUser) {
+      throw new Error("Este correo ya ha sido registrado");
+    }
+
+    // Encrypt password
+    const encryptedPassword = await bcrypt.hash(data.password, 10);
+    console.log(encryptedPassword);
+
+    // insert the user
+    const user = await database.createUser({
+      ...data,
+      password: encryptedPassword,
+    });
+
+    if (!user) {
+      throw new Error("Hubo un problema al crear el usuario");
+    }
+
+    const token = jwt.sign(
+      {
+        userId: user.id,
+      },
+      "OSIdn1o8sd7y=A(ShdosdnA?",
+      {
+        expiresIn: "2h",
+      }
+    );
+
+    user.token = token;
+    res.status(200).json(user);
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(500).json({ error: error.message });
+    }
+    throw error;
   }
-
-  // Encrypt password
-  const encryptedPassword = await bcrypt.hash(data.password, 10);
-  console.log(encryptedPassword)
-
-  // insert the user
-  const user = await database.createUser({
-    ...data,
-    password: encryptedPassword,
-  });
-  res.status(200).json(user);
 };
